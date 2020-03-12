025fadd925b57a60cd86a69a62ed95dadcde379d
iballi2017 <iballi2017@gmail.com>
2020-03-12 17:32:36 +0100

    commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
-$(document).ready(function(){
-    $('.carousel').carousel({
-        interval: 7000
-      })
+$(document).ready(function () {
+  $('.carousel').carousel({
+    interval: 7000
+  })
 
-      
-      const myLazyLoad = new LazyLoad({
-        elements_selector: ".lazy-image"
-   });
-   myLazyLoad();
-   
+
+  const myLazyLoad = new LazyLoad({
+    elements_selector: ".lazy-image"
+  });
+  myLazyLoad();
+
+})
+
+
+window.addEventListener("load", () => {
+  const preload = document.querySelector(".preload")
+  preload.classList.add("preload-finish")
 })
