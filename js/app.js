3186485c390e22c813dd7dc259596fc34632f9f3
iballi2017 <iballi2017@gmail.com>
2020-09-24 18:33:42 +0100

    Editing the loading effect

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
-$(document).ready(function () {
-  $('.carousel').carousel({
-    interval: 7000
-  })
+$(document).ready(function() {
+    $('.carousel').carousel({
+        interval: 7000
+    })
 
 
-  const myLazyLoad = new LazyLoad({
-    elements_selector: ".lazy-image"
-  });
-  myLazyLoad();
-
-
+    const myLazyLoad = new LazyLoad({
+        elements_selector: ".lazy-image"
+    });
+    myLazyLoad;
 })
 
 
 /* .......Page loader.......*/
-$(window).on("load", function () {
-  $(".preload").hide("slow")
-  // $(".preload").addClass("preload-finish")    //this works as well
+$(window).on("load", function() {
+    $(".preload").hide("slow")
+        // console.log($(".preload").siblings())
+    $(".preload").siblings().css({ "display": "block" })
+        // $(".preload").addClass("preload-finish")    //this works as well
 })
 
 /* .......youtube video loader.......*/
-$(".youtube-vid").on("load", function () {
-  $(".preload-video").hide("slow")
+$(".youtube-vid").on("load", function() {
+    $(".preload-video").hide("slow")
 })
 
 // section scroll animation library
 AOS.init({
-  duration: 1200,
+    duration: 1200,
 })
